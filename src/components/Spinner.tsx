ef7c3c1cee1e389a0f1e05da41d931848339ed23
jazzyfact <jazzyfact2000@gmail.com>
2024-10-12 19:12:44 +0900

    [add] 스피너 추가

---
+++ src/components/Spinner.tsx
@@ -0,0 +1,11 @@
+import '../scss/spinner.scss'
+
+const Spinner = () => {
+    return (
+        <div className="spinner">
+            <div className="loader"></div>
+        </div>
+    )
+}
+
+export default Spinner
