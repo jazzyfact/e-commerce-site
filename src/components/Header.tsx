28452238b07d6b4ae095456032b71a1fe5aefa10
jazzyfact <jazzyfact2000@gmail.com>
2024-10-12 15:20:18 +0900

    [add] 홈,장바구니 헤더 부분 레이아웃 추가

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { BrowserRouter, Link } from 'react-router-dom'
+import '../scss/header.scss'
+
+export const Header = () => {
+    return (
+        <BrowserRouter>
+            <header>
+                <nav>
+                    <Link to="/">홈</Link>
+                    <Link to="/cart">장바구니</Link>
+                </nav>
+            </header>
+        </BrowserRouter>
+    )
+}
