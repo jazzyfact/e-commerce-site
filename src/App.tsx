4a3dd404d2ebef74fe6b7ded46ee222a48e05d0c
jazzyfact <jazzyfact2000@gmail.com>
2024-10-12 19:21:23 +0900

    [add] 라우터 설정, 홈, 장바구니 클릭시 페이지 이동

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
+// App.tsx
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Home from './page/Home'
+import CartPage from './page/CartPage'
 
 function App() {
     return (
-        <>
+        <BrowserRouter>
             <Header />
-            <Home />
-        </>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/cart" element={<CartPage />} />
+            </Routes>
+        </BrowserRouter>
     )
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,15 @@
-import React from 'react'
-import { BrowserRouter, Link } from 'react-router-dom'
+// Header.tsx
+import { Link } from 'react-router-dom'
 import '../scss/header.scss'
 
 const Header = () => {
     return (
-        <BrowserRouter>
-            <header>
-                <nav>
-                    <Link to="/">홈</Link>
-                    <Link to="/cart">장바구니</Link>
-                </nav>
-            </header>
-        </BrowserRouter>
+        <header>
+            <nav>
+                <Link to="/">홈</Link>
+                <Link to="/cart">장바구니</Link>
+            </nav>
+        </header>
     )
 }
 
